094ab437de5eb29bdaae845c72e8edbd7ad35348
utp0 <paksyh15@gmail.com>
2025-01-15 22:16:09 +0100

    perm helper into separate file

--- app/src/main/java/com/example/camtest2/MainActivity.java
+++ app/src/main/java/com/example/camtest2/MainActivity.java
@@ -1,36 +1,23 @@
 package com.example.camtest2;
 
-import android.Manifest;
 import android.content.ContentResolver;
 import android.content.ContentValues;
 import android.content.Context;
-import android.content.pm.PackageManager;
 import android.graphics.BitmapFactory;
-import android.hardware.camera2.CameraAccessException;
 import android.hardware.camera2.CameraManager;
 import android.net.Uri;
-import android.os.Build;
 import android.os.Bundle;
 
-import androidx.activity.EdgeToEdge;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.app.ActivityCompat;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 import android.hardware.Camera;
-import android.os.Bundle;
 import android.provider.MediaStore;
 import android.util.Log;
-import android.view.SurfaceControl;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
 import android.view.View;
 import android.widget.Toast;
-
-import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
@@ -45,33 +32,33 @@ public class MainActivity extends AppCompatActivity {
 
     SurfaceHolder.Callback callback;
 
+    private int getAvailableCameraId() {
+        CameraManager manager = (CameraManager) getSystemService(Context.CAMERA_SERVICE);
+        try {
+            String[] cameras = manager.getCameraIdList();
+            for (String camera : cameras) {
+                if (!camera.equals("front")) {
+                    return Integer.parseInt(camera);
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace(); // TODO: értelmesebben
+        }
+        return -1;
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        // check cam perms
-
+        // permissions
         try {
-
-            // perms
-            if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED || ActivityCompat.checkSelfPermission(this, android.Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
-
-                ActivityCompat.requestPermissions(MainActivity.this, new String[]{Manifest.permission.READ_EXTERNAL_STORAGE, Manifest.permission.WRITE_EXTERNAL_STORAGE}, 0);
-
-            }
-            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
-                if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.READ_MEDIA_IMAGES) != PackageManager.PERMISSION_GRANTED || ActivityCompat.checkSelfPermission(this, android.Manifest.permission.READ_MEDIA_VIDEO) != PackageManager.PERMISSION_GRANTED) {
-                    ActivityCompat.requestPermissions(MainActivity.this, new String[]{Manifest.permission.READ_MEDIA_IMAGES, Manifest.permission.READ_MEDIA_VIDEO}, 1);
-                }
-
-            }
-
-            CameraManager manager = (CameraManager) getSystemService(Context.CAMERA_SERVICE);
-            String cameraId = manager.getCameraIdList()[0];
-        } catch (CameraAccessException e) {
-            Log.e("TAG", "Cannot access the camera.", e);
+            PermissionsHelper.makeSurePerms(this);
+        } catch (Exception e) {
+            Log.e(this.getClass().getName(), "Cannot access the camera.", e);
         }
 
+        // set the view
         setContentView(R.layout.activity_main);
 
         // Find the SurfaceView and request a Surface from it.
@@ -82,13 +69,12 @@ public class MainActivity extends AppCompatActivity {
         if (cameraId == -1) {
             cameraId = getAvailableCameraId();
         }
-
         camera = Camera.open(cameraId);
 
         // Set up a SurfaceHolder callback to be notified when the surface is available.
         SurfaceHolder.Callback callback = new SurfaceHolder.Callback() {
             @Override
-            public void surfaceCreated(SurfaceHolder surfaceHolder) {
+            public void surfaceCreated(@NonNull SurfaceHolder surfaceHolder) {
                 // The surface has been created, assign it to the surface holder.
 
                 try {
@@ -102,7 +88,7 @@ public class MainActivity extends AppCompatActivity {
                 } catch (Exception e) {
 
                     Toast.makeText(MainActivity.this, "Error starting preview:" + e.getMessage(), Toast.LENGTH_SHORT).show();
-                    e.printStackTrace(); // TODO: értelmesebben
+                    throw new RuntimeException(e); // TODO: értelmesebben
                 }
             }
 
@@ -162,7 +148,7 @@ public class MainActivity extends AppCompatActivity {
                                 "",
                                 "")
                         );
-                        if(uri != null) {
+                        if (uri != null) {
                             camera.stopPreview();
                             camera.startPreview();
                         }
@@ -172,21 +158,6 @@ public class MainActivity extends AppCompatActivity {
                 camera.takePicture(shutterCallback, rawCallback, postviewCallback, jpegCallback);
             }
         });
-    }
-
-    private int getAvailableCameraId() {
-        CameraManager manager = (CameraManager) getSystemService(Context.CAMERA_SERVICE);
-        try {
-            String[] cameras = manager.getCameraIdList();
-            for (String camera : cameras) {
-                if (!camera.equals("front")) {
-                    return Integer.parseInt(camera);
-                }
-            }
-        } catch (Exception e) {
-            e.printStackTrace(); // TODO: értelmesebben
-        }
-        return -1;
     }
 
     @Override
---
+++ app/src/main/java/com/example/camtest2/PermissionsHelper.java
@@ -0,0 +1,41 @@
+package com.example.camtest2;
+
+import android.Manifest;
+import android.app.Activity;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.hardware.camera2.CameraAccessException;
+import android.hardware.camera2.CameraManager;
+import android.os.Build;
+import android.util.Log;
+
+import androidx.core.app.ActivityCompat;
+
+public class PermissionsHelper {
+
+    public static void makeSurePerms(Context context) throws Exception {
+        // check cam perms
+
+        try {
+
+            // perms
+            if (ActivityCompat.checkSelfPermission(context, android.Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED || ActivityCompat.checkSelfPermission(context, android.Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+
+                ActivityCompat.requestPermissions((Activity) context, new String[]{android.Manifest.permission.READ_EXTERNAL_STORAGE, android.Manifest.permission.WRITE_EXTERNAL_STORAGE}, 0);
+
+            }
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+                if (ActivityCompat.checkSelfPermission(context, android.Manifest.permission.READ_MEDIA_IMAGES) != PackageManager.PERMISSION_GRANTED || ActivityCompat.checkSelfPermission(context, android.Manifest.permission.READ_MEDIA_VIDEO) != PackageManager.PERMISSION_GRANTED) {
+                    ActivityCompat.requestPermissions((Activity) context, new String[]{android.Manifest.permission.READ_MEDIA_IMAGES, Manifest.permission.READ_MEDIA_VIDEO}, 1);
+                }
+
+            }
+
+            CameraManager manager = (CameraManager) context.getSystemService(Context.CAMERA_SERVICE);
+            String cameraId = manager.getCameraIdList()[0];
+        } catch (CameraAccessException e) {
+            Log.e(PermissionsHelper.class.getName(), "Cannot get all permissions.", e);
+            throw new Exception(e);
+        }
+    }
+}
